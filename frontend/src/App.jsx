42d3a04e63252a105eab3d5843a83ad506475975
akhil <akhilpatyal49@gmail.com>
2025-03-17 18:59:53 +0530

    add admin section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
-import {Toaster} from "sonner"
+import { Toaster } from "sonner";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
@@ -12,25 +12,28 @@ import OrderConfirmation from "./pages/OrderConfirmation";
 import OrderDetailsPage from "./pages/OrderDetailsPage";
 import MyOrderPage from "./pages/MyOrderPage";
 import AdminLayout from "./components/Admin/AdminLayout";
+import AdminHomePage from "./pages/AdminHomePage";
 function App() {
   return (
-    <BrowserRouter future={{v7_startTransition:true}}>
-    <Toaster position="top-right"/>
+    <BrowserRouter future={{ v7_startTransition: true }}>
+      <Toaster position="top-right" />
       <Routes>
         <Route path="/" element={<UserLayout />}>
-          <Route index element={<Home />}/>
-          <Route path="login" element={<Login />}/>
-          <Route path="register" element={<Register />}/>
-          <Route path="profile" element={<Profile />}/>
-          <Route path="collections/:collection" element={<CollectionPage />}/>
-          <Route path="product/:id" element={<ProductDetails />}/>
-          <Route path="checkout" element={<CheckOut />}/>
-          <Route path="order-confirmation" element={<OrderConfirmation />}/>
-          <Route path="order/:id" element={<OrderDetailsPage />}/>
-          <Route path="/my-orders" element={<MyOrderPage />}/>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="collections/:collection" element={<CollectionPage />} />
+          <Route path="product/:id" element={<ProductDetails />} />
+          <Route path="checkout" element={<CheckOut />} />
+          <Route path="order-confirmation" element={<OrderConfirmation />} />
+          <Route path="order/:id" element={<OrderDetailsPage />} />
+          <Route path="/my-orders" element={<MyOrderPage />} />
         </Route>
-        
-          <Route path="/admin" element={<AdminLayout />}/>
+
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminHomePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
